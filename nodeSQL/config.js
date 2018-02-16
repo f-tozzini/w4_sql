8387c0a07197d3787a9a9ba46cbc5fa5bcfc7b1b
f-tozzini <31866568+f-tozzini@users.noreply.github.com>
2018-02-16 12:00:53 -0500

    first commit

---
+++ nodeSQL/config.js
@@ -0,0 +1,9 @@
+var config = {
+  user: "root",
+  password: "root",
+  port: 8889,
+  host: "localhost",
+  database: "cooperStuff"
+}
+
+module.exports = config;
---
+++ nodeSQL/public/javascripts/main.js
@@ -0,0 +1,34 @@
+(() => {
+  let getButtons = document.querySelectorAll('.getButton'),
+      deleteButton = document.querySelector('.deleteButton');
+
+  function fetchData(){
+    let url = "api/" + this.id;
+
+    fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  function deleteRecord(){
+    let url = "api/" + this.id;
+
+    fetch(url, {method : 'delete'})
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  getButtons.forEach (button => button.addEventListener('click', fetchData, false));
+  deleteButton.addEventListener('click', deleteRecord, false);
+
+})();
